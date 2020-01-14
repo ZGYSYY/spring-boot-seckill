9e9dcc95a80cb8555b7ff5428fae48b68aacbe34
ZGY <3030392760@qq.com>
2020-01-14 11:39:58 +0800

    修改代码

--- src/main/java/com/itstyle/seckill/queue/redis/RedisSubListenerConfig.java
+++ src/main/java/com/itstyle/seckill/queue/redis/RedisSubListenerConfig.java
@@ -31,7 +31,32 @@ public class RedisSubListenerConfig {
          * 如果不定义线程池，每一次消费都会创建一个线程，如果业务层面不做限制，就会导致秒杀超卖
          */
         ThreadFactory factory = new ThreadFactoryBuilder().setNameFormat("redis-listener-pool-%d").build();
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 2, 5L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(1000), factory);
+        /*
+         * corePoolSize 不要设置为 1，如果设置为 1，订阅线程和消费线程公用一个线程池，而该线程池只有一个核心线程。订阅线程在系统启动后
+         * 就已经开始占用了核心线程，导致消费线程获取不了执行权。所以不会被消费。
+         * 以下是源代码分析：
+         *
+         * ```java
+         * RedisMessageListenerContainer 类的如下方法
+         * public void afterPropertiesSet() {
+         * 		if (taskExecutor == null) {
+         * 			manageExecutor = true;
+         * 			taskExecutor = createDefaultTaskExecutor();
+         *                }
+         *
+         * 		if (subscriptionExecutor == null) {
+         * 			subscriptionExecutor = taskExecutor;
+         *        }
+         *
+         * 		initialized = true;
+         * }
+         * ```
+         *
+         * 如果非要设置为 1，就要设置添加如下点，定义一个订阅线程池
+         * container.setSubscriptionExecutor(Executors.newFixedThreadPool(1));
+         */
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 2, 5L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(1000), factory);
+        container.setSubscriptionExecutor(Executors.newFixedThreadPool(1));
         container.setTaskExecutor(executor);
         return container;
     }
